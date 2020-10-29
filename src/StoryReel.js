040cfb91a8fe991ff84891478127c0fbcaaa1399
Prashastha01 <kovuriprashastha01@gmail.com>
2020-10-29 21:06:52 +0530

    linkedin icons

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './StoryReel.css';
 import Story from './Story.js';
-import Img from './images/myImg.png';
-import Img2 from './images/default.jpg';
+// import Img from './images/myImg.png';
+// import Img2 from './images/default.jpg';
+import { Add } from '@material-ui/icons';
+import './Story.css';
+import { IconButton } from '@material-ui/core';
+import db from './firebase.js';
+import { useStateValue } from './StateProvider.js';
 
 export default function StoryReel(props) {
-    return (
-        <div className='storyReel'>
-            <Story image={Img2} profileSrc={Img} title='Prash'/>
-            <Story image={Img2} profileSrc={Img} title='Prash'/>
-            <Story image={Img2} profileSrc={Img} title='Prash'/>
-            <Story image={Img2} profileSrc={Img} title='Prash'/>
-            <Story image={Img2} profileSrc={Img} title='Prash'/>
-            <Story image={Img2} profileSrc={Img} title='Prash'/>
-            
-        </div>
-    )
+  const [stories, setStories] = useState([]);
+  useEffect(() => {
+    db.collection('stories').onSnapshot((snapshot) =>
+      setStories(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    );
+  }, []);
+
+  const [{ user }, dispatch] = useStateValue();
+  //   const [input, setInput] = useState('');
+  //   const [imageUrl, setImageUrl] = useState('');
+
+  const addStory = (e) => {
+    e.preventDefault();
+    db.collection('stories').add({
+      title: user.displayName,
+      profileSrc: user.photoURL,
+      image: user.photoURL,
+    });
+  };
+  return (
+    <div className='storyReel'>
+      <IconButton onClick={addStory}>
+        <Add className='story' />
+      </IconButton>
+      {stories.map((story) => (
+        <Story
+          key={story.id}
+          profileSrc={story.data.profileSrc}
+          image={story.data.image}
+          title={story.data.title}
+        />
+      ))}
+    </div>
+  );
 }
