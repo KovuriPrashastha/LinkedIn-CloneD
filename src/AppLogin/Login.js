fbc11545b813311866a324871ed8c23910b2918b
Prashastha01 <kovuriprashastha01@gmail.com>
2020-12-10 00:17:38 +0530

    final commit

--- src/AppLogin/Login.js
+++ src/AppLogin/Login.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Login.css';
-import logo1 from '../images/vision.jpg';
-import logo2 from '../images/mission.jpg';
 import logo from '../images/logo.png';
 import { auth, provider } from '../firebase.js';
 import { useStateValue } from '../StateProvider.js';
 import { actionTypes } from '../reducer.js';
-import { Button, Paper } from '@material-ui/core';
-import l1 from '../images/l1.png'
-import l3 from '../images/l3.png'
-
+import { Button, Paper, Avatar } from '@material-ui/core';
+import l1 from '../images/l1.png';
+import l3 from '../images/l3.png';
 
 function Login() {
   const [state, dispatch] = useStateValue();
@@ -32,43 +29,49 @@ function Login() {
     
        
       </div> */}
-          <div className='login__logo'>
+      <div className='login__logo'>
         <img src={logo} alt='' className='logos' />
       </div>
       <center>
-      <div>
-        <img src={l1} alt="" className="ill"/>
-      </div>
-      
-  
-      <Button  type='submit' onClick={signIn}>
-        Sign In
-      </Button>
-      <div>
-        <img className="ill" src={l3} alt=""/>
-      </div>
+        <div>
+          <img src={l1} alt='' className='ill' />
+        </div>
+
+        <Button
+          type='submit'
+          onClick={signIn}
+          className='signupButton'
+          style={{
+            'text-transform': 'none',
+            fontSize: '18px',
+            'padding-left': '40px',
+            'font-weight': 'bold',
+          }}
+        >
+          Sign In with Google
+        </Button>
+        <div>
+          <img className='ill' src={l3} alt='' />
+        </div>
       </center>
       <Paper className='paper2'>
         {/* <center>
             <img src={logo2} alt='' className='logos' />
             </center> */}
-        
-    
 
-          <h4 className='heading'>
-            "To arm young brains with competitive technology and nurture
-            <br />
-            holistic development of the individuals for a better tomorrow."
-          </h4>
-        </Paper>
-        <Paper className='paper1'>
-      
-      <h4 className='heading'>
-        "Striving for a symbiosis of technological excellence and human
-        <br />
-        values."
-      </h4>
-    </Paper>
+        <h4 className='heading'>
+          "To arm young brains with competitive technology and nurture
+          <br />
+          holistic development of the individuals for a better tomorrow."
+        </h4>
+      </Paper>
+      <Paper className='paper1'>
+        <h4 className='heading'>
+          "Striving for a symbiosis of technological excellence and human
+          <br />
+          values."
+        </h4>
+      </Paper>
     </div>
   );
 }
